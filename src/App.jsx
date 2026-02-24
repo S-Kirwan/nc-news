1c68801e5dc6754a6f5d44c5f318fdfac7f6150c
Sean Kirwan <seandarrakirwan@gmail.com>
2026-02-24 11:55:03 +0000

    Added React Routing
    Basic structure with general routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import Header from "./components/header.jsx";
+import Home from "./components/home.jsx";
+
+function App() {
+
+	const	defaultUser =
+	{
+		username:"jessjelly",
+		name:"Jess Jelly",
+		avatar_url:"https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141"
+	}
+
+	const [user, setUser] = useState(defaultUser);
+	
+	return (
+	  <>
+	  <div className="App">
+		<Header user={user} />
+	  	<Routes>
+	  		<Route path="/" element={<Home/>}/>
+	  	</Routes>
+	  </div>
+	  </>
+	)
 }
 
 export default App
