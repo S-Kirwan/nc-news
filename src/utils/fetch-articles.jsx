400ca6577461cde08d3a73615b11d76590862b55
Sean Kirwan <seandarrakirwan@gmail.com>
2026-02-25 11:26:37 +0000

    Added utils folder
    For functions used across components in useEffects.
    E.g fetchArticles

---
+++ src/utils/fetch-articles.jsx
@@ -0,0 +1,42 @@
+async function fetchLatestArticles ()
+{
+	const	url = "https://news-back-end-nqi7.onrender.com/api/articles";
+	const	maxArticles = 5;
+	
+	try
+	{
+		const	body = await fetch(url);
+	
+		const	parsedBody = await body.json();
+		console.log(parsedBody);
+		return (parsedBody.articles.slice(0, maxArticles));
+	
+	} catch (error)
+	{
+		console.log(error);
+		return ([]);
+	}
+}
+
+async function fetchArticles ()
+{
+	let url = "https://news-back-end-nqi7.onrender.com/api/articles";
+	
+	if (query !== null)
+		url += query;
+	
+	try
+	{
+		const	body = await fetch(url);
+	
+		const	parsedBody = await body.json();
+		
+		return (parsedBody.articles);
+	} catch (error)
+	{
+		console.log(error);
+		return ([]);
+	}
+}
+
+export { fetchLatestArticles, fetchArticles };
