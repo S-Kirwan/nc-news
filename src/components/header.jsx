12e3dcc9e294f875a0f06d4bfa0c7204136a6883
Sean Kirwan <seandarrakirwan@gmail.com>
2026-02-27 07:49:24 +0000

    Added link to header
    Takes home when clicking home title

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,15 @@
-import hamburgerMenuIcon from "../assets/hamburer-menu-icon.svg"
+import hamburgerMenuIcon from "../assets/hamburer-menu-icon.svg";
+
+import { Link } from "react-router-dom";
 
 function Header ( { user } )
 {
 	return (
 		<header className="Header">
 			<img className="hamburger-menu" src={hamburgerMenuIcon} alt="menu icon" />
-			<h1>Snowfall</h1>
+			<Link to="/">
+				<h1>Snowfall</h1>
+			</Link>
 			<section id="header-user-info">
 				<p id="header-username">{user.username}</p>
 				<img id="header-avatar" className="avatar-img" src={user.avatar_url} alt="user avatar" width="200" height="200" />
